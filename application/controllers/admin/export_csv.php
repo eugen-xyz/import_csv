f2574e71f4527302f72bfb1590b4bd10a13392fd
Eugene Grava <eugenegrava.c@gmail.com>
2016-08-27 15:52:48 +0800

    Controller for the csv files
    Signed-off-by: Eugene Grava <eugenegrava.c@gmail.com>

---
+++ application/controllers/admin/export_csv.php
@@ -0,0 +1,59 @@
+<?php 
+    class Export_csv extends CI_Controller{
+
+        public function __construct(){
+
+            parent::__construct();
+            $this->load->helper('url');
+        }
+
+        public function csv_resource(){
+            $jsonResource = base_url(). "index.php/admin/manhours/api";
+            $json = file_get_contents($jsonResource);
+            $array = json_decode($json, true);
+            $f = fopen('php://output', 'w');
+            $firstLineKeys = false;
+            foreach ($array as $line)
+            {
+                if (empty($firstLineKeys))
+                {
+                    $firstLineKeys = array_keys($line);
+                    fputcsv($f, $firstLineKeys);
+                    $firstLineKeys = array_flip($firstLineKeys);
+                }
+                fputcsv($f, array_merge($firstLineKeys, $line));
+            }
+        }
+
+        public function save(){
+
+            $json_resource = base_url(). "index.php/admin/manhours/api";
+            $get_json_resource =  file_get_contents($json_resource) ;
+            $decode_json_resource = json_decode($get_json_resource, true);
+
+            //get the api for the filename
+            // must be the user_id from session
+            $first =  $decode_json_resource[0]['first_name'];
+            $last =  $decode_json_resource[0]['last_name'];
+
+            $filename = $first.$last;
+
+            $path = base_url(). "index.php/admin/export/csv";
+            $file =  file_get_contents($path) ;
+
+
+            $handle = fopen($filename.".csv", "w");
+            fwrite($handle, $file);
+            fclose($handle);
+
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($filename.'.csv'));
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filename.'.csv'));
+            readfile($filename.'.csv');
+            exit;
+ 
+        }
+    }
